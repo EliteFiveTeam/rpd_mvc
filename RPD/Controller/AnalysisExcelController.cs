fe752468750b5083f8fe556ca08bce8789af9e44
Mikhail Gorshenin <m.gorshenin@profsoft.pro>
2019-01-31 01:44:15 +0400

    fix last commit

--- RPD/Controller/AnalysisExcelController.cs
+++ RPD/Controller/AnalysisExcelController.cs
@@ -10,11 +10,14 @@ namespace RPD.Controller
 {
     public class AnalysisExcelController
     {
-        private IAnalysisExcelForm _view;
+        private IAnalysisExcelForm _view; 
+
         public AnalysisExcelController(AnalysisExcelForm view)
         {
             _view = view;
             _view.SetController(this);
         }
+
+
     }
 }
